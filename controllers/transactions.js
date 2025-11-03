ce45809565662b638d08b342f8cf113d21742da5
David Kiama <davidkiama98@gmail.com>
2025-11-03 16:07:28 +0300

    Commit

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -107,8 +107,8 @@ export const deposit = async (req, res) => {
     // Return the URL to the client
     res.status(200).json({ paymentUrl });
   } catch (error) {
-    console.error("paymentUrl", paymentUrl);
-    console.error("response", response);
+    console.error("paymentUrl", OXAPAY_CREATE_INVOICE_URL, data, { headers });
+    // console.error("response", response);
     console.error("Deposit Error:", error.response?.data || error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
