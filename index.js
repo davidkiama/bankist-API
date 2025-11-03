7834bfb772d9e2967bd46826f4abb79f870c06f6
David Kiama <davidkiama98@gmail.com>
2025-11-03 15:23:53 +0300

    revert api to deploy

--- index.js
+++ index.js
@@ -1,56 +1,34 @@
-// import express from "express";
-// import cors from "cors";
-// import bodyParser from "body-parser";
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-
-// import authRoutes from "./routes/auth.js";
-// import transactionRoutes from "./routes/transactions.js";
-
-// const app = express();
-// dotenv.config();
-
-// // middlewares
-// app.use(cors());
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-
-// app.use("/auth", authRoutes);
-// app.use("/transaction", transactionRoutes);
-
-// // connect to DB
-
-// const CONNECTION_URL = process.env.CONNECTION_URL;
-// const PORT = process.env.PORT || 5000;
-
-// app.get("/", (req, res) => {
-//   res.send("APP IS RUNNING");
-// });
-
-// mongoose
-//   .connect(CONNECTION_URL)
-//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-//   .catch((error) => console.log(`${error} did not connect`));
-
 import express from "express";
-import serverless from "serverless-http";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-import { connectToDB } from "../db.js";
-import authRoutes from "../routes/auth.js";
-import transactionRoutes from "../routes/transactions.js";
+import authRoutes from "./routes/auth.js";
+import transactionRoutes from "./routes/transactions.js";
 
 const app = express();
+dotenv.config();
 
+// middlewares
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
 app.use("/auth", authRoutes);
 app.use("/transaction", transactionRoutes);
 
-app.get("/", async (req, res) => {
-  await connectToDB(); // ensure DB is connected
+// connect to DB
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
   res.send("APP IS RUNNING");
 });
 
-export default serverless(app);
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
+  )
+  .catch((error) => console.log(`${error} did not connect`));
